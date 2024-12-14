fcd827e4c9b62e33c8104c1815e90f256c70de1d
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2024-12-14 13:11:06 +0100

    add: integrated map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.retrofit)
     implementation(libs.squareup.converter.gson)
+    implementation (libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/htt/catalogo/ui/map/MapFragment.kt
+++ app/src/main/java/htt/catalogo/ui/map/MapFragment.kt
@@ -5,16 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import htt.catalogo.R
 
-class MapFragment : Fragment()  {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.activity_map, container, false)
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment_container) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+
+        val location = LatLng(37.1773, -3.5986)
+        googleMap.addMarker(MarkerOptions().position(location).title("Granada"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 10f))
     }
 }
